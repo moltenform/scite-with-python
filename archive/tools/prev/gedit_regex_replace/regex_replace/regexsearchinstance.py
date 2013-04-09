5f0bf1f832328f9770ceb1457ba449d466f6f657
ben fisher <jamon.ben@gmail.com>
2013-04-08 23:33:48 -0700

    my improvements

--- archive/tools/prev/gedit_regex_replace/regex_replace/regexsearchinstance.py
+++ archive/tools/prev/gedit_regex_replace/regex_replace/regexsearchinstance.py
@@ -8,9 +8,9 @@ import re
 ui_str = """
 <ui>
     <menubar name="MenuBar">
-        <menu name="ToolsMenu" action="Tools">
-            <placeholder name="ToolsOps_2">
-                <menuitem name="Regex Search" action="RegexSearch"/>
+        <menu name="SearchMenu" action="Search">
+            <placeholder name="SearchOps_3">
+                <menuitem name="Regular expression..." action="RegexSearch"/>
             </placeholder>
         </menu>
     </menubar>
@@ -33,7 +33,7 @@ class RegexSearchInstance:
     # Create menu item 
     # Create our menu item in "Tools" menu.
     def create_menu_item(self):
-        action = gtk.Action("RegexSearch", _("Regex Search"), _("Search using regular expressions"), None)
+        action = gtk.Action("RegexSearch", _("Regular expression..."), _("Search using regular expressions"), None)
         action.connect("activate", self.on_open_regex_dialog)
 
         action_group = gtk.ActionGroup("RegexSearchActions")
@@ -63,6 +63,8 @@ class RegexSearchInstance:
 
         self._replace_button = glade_xml.get_widget("replace_button")
         self._replace_button.connect("clicked", self.on_replace_button_clicked)
+        self._replace_all_button = glade_xml.get_widget("replace_all_button")
+        self._replace_all_button.connect("clicked", self.on_replace_all_button_clicked)
 
         close_button = glade_xml.get_widget("close_button")
         close_button.connect("clicked", self.on_close_button_clicked)
@@ -74,6 +76,8 @@ class RegexSearchInstance:
         self._replace_text_box.connect("changed", self.on_replace_text_changed)
 
         self._wrap_around_check = glade_xml.get_widget("wrap_around_check")
+        self._use_backreferences_check = glade_xml.get_widget("use_backreferences_check")
+        self._case_sensitive_check = glade_xml.get_widget("case_sensitive_check")
 
 
     ###
@@ -86,14 +90,49 @@ class RegexSearchInstance:
     def on_replace_button_clicked(self, replace_button):
         self.search_document(button = 'replace')
 
+    # Called when the "Replace All" button is clicked.
+    def on_replace_all_button_clicked(self, replace_button):
+        document = self._window.get_active_document()
+        start_iter = document.get_start_iter()
+        end_iter = document.get_end_iter()
+        alltext = unicode(document.get_text(start_iter, end_iter, False), "utf-8")
+        
+        regex = self.create_regex()
+        if regex==None: return
+        
+        replace_string = self._replace_text_box.get_text()
+        if not self._use_backreferences_check.get_active():
+            replace_string = replace_string.replace('\\','\\\\') # turn \ into \\ so that backreferences are not done.
+        
+        new_string, n_replacements = regex.subn(replace_string, alltext)
+        
+        selection_bound_mark = document.get_mark("selection_bound")
+        document.place_cursor(start_iter)
+        document.move_mark(selection_bound_mark, end_iter)
+        document.delete_selection(False, False)
+        document.insert_at_cursor(new_string)
+      
+        self.show_alert_dialog(u"%d replacement(s)." % (n_replacements))
+
     ###
     # Called when the "Close" button is clicked.
     def on_close_button_clicked(self, close_button):
         self._search_dialog.hide()
 
+    def create_regex(self):
+        try:
+            # note multi-line flag, and dot does not match newline.
+            if self._case_sensitive_check.get_active():
+                regex = re.compile( unicode(self._search_text_box.get_text(), "utf-8"), re.MULTILINE)
+            else:
+                regex = re.compile( unicode(self._search_text_box.get_text(), "utf-8"), re.MULTILINE | re.IGNORECASE)
+        except:
+            self.show_alert_dialog(u"Invalid regular expression.")
+            return None
+        return regex
 
     ###
-    # Called when the text to be searched is changed.
+    # Called when the text to be searched is changed. We enable the fields once. (still want to be able to replace '')
     def on_search_text_changed(self, search_text_entry):
         search_text  = search_text_entry.get_text()
         replace_text_entry = self._replace_text_box
@@ -108,13 +147,14 @@ class RegexSearchInstance:
     ###
     # Called when the text to be replaced is changed.
     def on_replace_text_changed(self, replace_text_entry):
-        replace_text = replace_text_entry.get_text()
-        search_text  =  self._search_text_box.get_text() 
-
-        if len(search_text) > 0 and len(replace_text) > 0:
-            self._replace_button.set_sensitive(True)
-        else:
-            self._replace_button.set_sensitive(False)
+        if not self.enable_replace:
+            replace_text = replace_text_entry.get_text()
+            search_text  =  self._search_text_box.get_text()
+            
+            if len(search_text) > 0 and len(replace_text) > 0:
+                self._replace_button.set_sensitive(True)
+                self._replace_all_button.set_sensitive(True)
+                self.enable_replace = True
 
     ###
     # To update plugin's user interface
@@ -125,6 +165,7 @@ class RegexSearchInstance:
     ###
     # Called to open the Regex Search dialog.
     def on_open_regex_dialog (self, action = None):
+        self.enable_replace = False
         self._search_dialog.show()
 
 
@@ -140,10 +181,10 @@ class RegexSearchInstance:
 
         end_iter = document.get_end_iter()
 
+        regex = self.create_regex()
+        if regex==None: return
+            
         text = unicode(document.get_text(start_iter, end_iter, False), "utf-8")
-
-        regex = re.compile(unicode(self._search_text_box.get_text(), "utf-8"))
-
         result = regex.search(text)
 
         if result != None:
@@ -158,18 +199,16 @@ class RegexSearchInstance:
                 self.search_document(document.get_start_iter(), True,button)
             else:
                 # We've already wrapped around. There's no match in the whole document.
-                self.inform_no_match()
+                self.show_alert_dialog(u"No match found for regular expression \"%s\"." % self._search_text_box.get_text())
 
 
-    ###
-    # Inform no match
-    # Inform the user that no match has been found for the specified regular expression.
-    def inform_no_match(self):
+
+    def show_alert_dialog(self, s):
         dlg = gtk.MessageDialog(self._window,
                                 gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
                                 gtk.MESSAGE_INFO,
                                 gtk.BUTTONS_CLOSE,
-                                _(u"The regular expression \"%s\" hasn't any match." % self._search_text_box.get_text()))
+                                _(s))
         dlg.run()
         dlg.hide()
 
@@ -185,16 +224,22 @@ class RegexSearchInstance:
         selection_bound_mark = document.get_mark("selection_bound")
         selection_bound_iter = document.get_iter_at_mark(selection_bound_mark)
 
-        # If our result is already selected, we will search again starting from the end of
-        # of the current result.
-        if start_iter.get_offset() + result.start() == curr_iter.get_offset() and \
-           start_iter.get_offset() + result.end() == selection_bound_iter.get_offset():
+        if button=='search':
+            # If our result is already selected, we will search again starting from the end of
+            # of the current result.
+            if start_iter.get_offset() + result.start() == curr_iter.get_offset() and \
+               start_iter.get_offset() + result.end() == selection_bound_iter.get_offset():
 
-            start_iter.forward_chars(result.end()+1) # To the first char after the current selection/match.
+                start_iter.forward_chars(result.end()+1) # To the first char after the current selection/match.
 
-            if start_iter.get_offset() < document.get_end_iter().get_offset() and not wrapped_around:
-                self.search_document(start_iter,False,button)
+                # fixed bug- no wrapping when match at end of document, used to be get_offset() < document
+                if start_iter.get_offset() <= document.get_end_iter().get_offset() and not wrapped_around:
+                    self.search_document(start_iter,False,button)
+            else:
+                self.show_search_result(result, document, start_iter, button)
         else:
+            # If we are replacing, and there is a selection that matches, we want to replace the selection.
+            # don't advance the cursor
             self.show_search_result(result, document, start_iter, button)
 
     ###
@@ -213,13 +258,19 @@ class RegexSearchInstance:
         document.move_mark(selection_bound_mark, result_end_iter)
 
         if (button == 'replace'):
-
             replace_text = self._replace_text_box.get_text()
-            self.replace_text(document,replace_text)
+            self.replace_text(document,replace_text, result)
 
         view = self._window.get_active_view()
         view.scroll_to_cursor()
 
-    def replace_text(self,document,replace_text):
+    def replace_text(self,document,replace_string, result):
+        if not self._use_backreferences_check.get_active():
+            replace_text = replace_string
+        else:
+            replace_text = result.expand(replace_string) # perform backslash expansion, like \1
         document.delete_selection(False, False)
         document.insert_at_cursor(replace_text)
+        
+        #now select the text that was replaced
+        
