86d10957e1d935fb4d4dbe6e77e4978d42fdf6ef
ben fisher <jamon.ben@gmail.com>
2013-04-30 23:28:38 -0700

    newline and tab normalization

--- archive/tools/prev/gedit_regex_replace/regex_replace/__init__.py
+++ archive/tools/prev/gedit_regex_replace/regex_replace/__init__.py
@@ -11,12 +11,12 @@ class RegexSearch(gedit.Plugin):
     def activate(self, window):
         regexsearch_instance = RegexSearchInstance(window)
         window.set_data(self.DATA_TAG, regexsearch_instance)
-	
+    
     def deactivate(self, window):
         regexsearch_instance = window.get_data(self.DATA_TAG)
         # regexsearch_instance destroy!?
         window.set_data(self.DATA_TAG, None)
-		
+        
     def update_ui(self, window):
         regexsearch_instance = window.get_data(self.DATA_TAG)
         regexsearch_instance.update_ui()
