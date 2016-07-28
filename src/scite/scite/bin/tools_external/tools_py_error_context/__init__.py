dc9ff77b6fa31bba7f73b9ad04f8ba28e8afc805
Ben Fisher <scitewiki@gmail.com>
2016-07-28 14:15:18 -0700

    Adding little plugin for quick Python debugging.

---
+++ src/scite/scite/bin/tools_external/tools_py_error_context/__init__.py
@@ -0,0 +1,28 @@
+
+
+def WritePrintStatement(word):
+    from scite_extend_ui import ScApp, ScEditor
+    whatToWrite = "print '''%s''', %s"%(word, word)
+    
+    ScEditor.BeginUndoAction()
+    try:
+        ScEditor.LineEnd()
+        ScEditor.NewLine()
+        ScEditor.PaneWrite(whatToWrite)
+    finally:
+        ScEditor.EndUndoAction()
+
+def AddPrintStatement():
+    from scite_extend_ui import ScApp, ScEditor
+    if ScEditor.GetFocus():
+        sel = ScEditor.GetSelectedText()
+        if sel and sel.strip():
+            if '\n' in sel or '\r' in sel:
+                print('Select within one line.')
+            else:
+                WritePrintStatement(sel)
+        else:
+            word = ScApp.GetProperty('CurrentWord')
+            if word and word.strip():
+                WritePrintStatement(word)
+            
