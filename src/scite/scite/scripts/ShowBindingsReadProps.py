b46823c10cb8f922a03a42abf69dcc0a09d01929
Ben Fisher <scitewiki@gmail.com>
2016-08-01 12:22:08 -0700

    Refactoring, script should work equally well on scite mainline

--- src/scite/scite/scripts/ShowBindingsReadProps.py
+++ src/scite/scite/scripts/ShowBindingsReadProps.py
@@ -8,9 +8,6 @@ class PropSetFile(object):
 		self.condition = None
 		self.platform = platform
 
-	def Set(self, key, val):
-		self.props[key] = val
-
 	def GetString(self, key):
 		return self.props.get(key, '')
 
@@ -18,28 +15,19 @@ class PropSetFile(object):
 		s = self.Expanded(self.GetString(key))
 		return int(s) if s else default
 
-	def ReadOneFile(self, filename):
-		self.ReadString(readall(filename, 'rb'))
-
-	def Evaluate(self, key):
-		if ' ' in key:
-			raise RuntimeError("We don't yet support functions like $(expand )")
-		else:
-			return self.GetString(key)
-
 	def _expandOnce(self, s):
 		if s.startswith('$(') and s.endswith(')'):
 			propname = s[2:-1]
-			return self.Evaluate(propname)
+			assert ' ' not in propname, "We don't yet support $(expand) etc."
+			return self.GetString(propname)
 		elif '$(' in s:
-			raise RuntimeError("We don't yet support expressions containing $() " + s)
+			assert False, "We don't yet support expressions containing $() " + s
 		else:
 			return s
 
 	def Expanded(self, s):
-		maxIters = 100
 		i = 0
-		while '$(' in s and i < maxIters:
+		while '$(' in s and i < 200:
 			s = self._expandOnce(s)
 			i += 1
 		return s
@@ -52,16 +40,15 @@ class PropSetFile(object):
 			if line.endswith('\\'):
 				s = s[0:-1]
 			else:
-				self._ReadLine(s)
+				self._readLine(s)
 				s = ''
 		
-		self._ReadLine(s)
-			
-	def _ReadLine(self, s):
+		self._readLine(s)
+
+	def _readLine(self, s):
 		if s.startswith('#') or not s.strip():
 			return
-			
-		if s.startswith('if PLAT_WIN'):
+		elif s.startswith('if PLAT_WIN'):
 			self.condition = 'win32'
 			return
 		elif s.startswith('if PLAT_MAC'):
@@ -71,7 +58,7 @@ class PropSetFile(object):
 			self.condition = 'gtk'
 			return
 		elif s.startswith('if '):
-			raise RuntimeError("Unsupported conditional" + s)
+			raise RuntimeError("Unsupported conditional " + s)
 			
 		if self.condition:
 			if s[0] == '\t':
@@ -82,55 +69,20 @@ class PropSetFile(object):
 			else:
 				self.condition = None
 		
-		# we don't yet support sections
-		if s[0] == '[':
-			return
-		
-		# we don't yet run import
-		if s.startswith('import '):
-			return
-		
-		try:
+		# we don't yet support sections or import
+		if s[0] != '[' and not s.startswith('import '):
 			key, val = s.split('=', 1)
-			self.Set(key, val)
-		except:
-			print('Encountered exception for line "%s"' % s)
-			raise
-			
-def getAllProperties(dir, platform):
+			self.props[key] = val
+
+def getAllProperties(dir, platform, fnCheckPath=None):
 	props = PropSetFile(platform)
 	for (path, dirs, files) in os.walk(dir):
-		if not path.endswith(os.sep + 'disabled') and not path.endswith(os.sep + 'tools_personal') and \
-			'tools_example' not in path:
+		if not fnCheckPath or fnCheckPath(path):
 			for file in files:
 				if file.endswith('.properties'):
 					full = os.path.join(path, file)
-					props.ReadOneFile(full)
+					props.ReadString(readall(full))
 	return props
-
-def takeBatch(iterable, size):
-	import itertools
-	it = iter(iterable)
-	item = list(itertools.islice(it, size))
-	while item:
-		yield item
-		item = list(itertools.islice(it, size))
-
-def warn(prompt):
-	print(prompt)
-	while True:
-		s = getInput('Continue? y/n')
-		if s == 'y':
-			break
-		elif s == 'n':
-			raise RuntimeError('chose not to continue')
-
-def getInput(prompt):
-	import sys
-	if sys.version_info[0] <= 2:
-		return raw_input(prompt)
-	else:
-		return input(prompt)
 
 def readShortcutLanguageMenu(results, props, key):
 	value = props.GetString(key)
@@ -139,55 +91,32 @@ def readShortcutLanguageMenu(results, props, key):
 		keyPress = props.Expanded(keyPress)
 		if keyPress.strip():
 			command = 'set language ' + languageName.replace('&', '')
-			binding = KeyBinding('properties *language', command, priority=40, platform='any')
-			binding.setKeyFromString(keyPress)
-			results.append(binding)
+			results.append(KeyBinding('properties *language', command, keyPress, priority=40, platform='any'))
 
-def addBindingFromCommand(results, props, key, keyCommandName, platform):
-	command = props.GetString(keyCommandName)
-	if command:
-		shortcut = props.GetString(key)
-		if shortcut.strip():
-			binding = KeyBinding('properties command', command, priority=50, platform=platform)
-			binding.setKeyFromString(shortcut)
-			results.append(binding)
-
-def readImplicitShortcutFromCommand(results, props, key):
-	matchObj = re.match(r'^command\.name\.([0-9])\.([^=]+)', key)
+def readShortcutFromCommand(results, props, key):
+	matchObj = re.match(r'^command\.name\.([0-9]+)\.([^=]+)', key)
 	if matchObj:
 		number = matchObj.group(1)
 		filetypes = matchObj.group(2)
 		name = props.Expanded(props.GetString(key))
+		platform = props.Expanded(filetypes)
+		platform = 'any' if platform == '*' else platform
 		setShortcutKey = 'command.shortcut.' + number + '.' + filetypes
-		if name and not props.GetString(setShortcutKey):
-			platform = props.Expanded(filetypes)
-			platform = 'any' if platform in ('*', '*.*') else platform
+		setShortcutValue = props.GetString(setShortcutKey)
+		if name and len(number) == 1 and not setShortcutValue:
 			binding = KeyBinding('properties command (implicit)', name, priority=50, platform=platform)
 			binding.keyChar = number
 			binding.control = True
 			results.append(binding)
-
-def readCommandShortcut(results, props, key):
-	cmd, shtcut, number, filetypes = key.split('.', 3)
-	platform = props.Expanded(filetypes)
-	platform = 'any' if platform in ('*', '*.*') else platform
-	keyCommandName = 'command.name.' + number + '.' + filetypes
-	addBindingFromCommand(results, props, key, keyCommandName, platform=platform)
-
-def readCustomCommandShortcut(results, props, key):
-	keyParts = key.split('.')
-	keyCommandName = '.'.join(keyParts[0:-1]) + '.name'
-	addBindingFromCommand(results, props, key, keyCommandName, platform='any')
+		elif name and setShortcutValue:
+			results.append(KeyBinding('properties command', name, setShortcutValue, priority=50, platform=platform))
 
 def readPropertiesUserShortcuts(results, props, key):
 	value = props.GetString(key)
 	parts = value.split('|')
-	for pair in takeBatch(parts, 2):
+	for pair in takePairs(parts):
 		if pair and len(pair) == 2:
-			shortcut, command = pair
-			binding = KeyBinding('properties user.shortcuts', command, priority=60, platform='any')
-			binding.setKeyFromString(shortcut)
-			results.append(binding)
+			results.append(KeyBinding('properties user.shortcuts', pair[1], pair[0], priority=60, platform='any'))
 
 def readFromProperties(props):
 	results = []
@@ -196,16 +125,12 @@ def readFromProperties(props):
 			readShortcutLanguageMenu(results, props, key)
 		elif key == 'user.shortcuts':
 			readPropertiesUserShortcuts(results, props, key)
-		elif key.startswith('command.shortcut.'):
-			readCommandShortcut(results, props, key)
 		elif key.startswith('command.name.'):
-			readImplicitShortcutFromCommand(results, props, key)
-		elif key.startswith('customcommand.') and key.endswith('.shortcut'):
-			readCustomCommandShortcut(results, props, key)
+			readShortcutFromCommand(results, props, key)
 	return results
 
 class KeyBinding(object):
-	def __init__(self, setName, command, priority, platform):
+	def __init__(self, setName, command, shortcut=None, priority=0, platform='all'):
 		self.control = False
 		self.alt = False
 		self.shift = False
@@ -214,8 +139,11 @@ class KeyBinding(object):
 		self.priority = priority
 		self.platform = platform
 		self.setName = setName
+		if shortcut:
+			self.setKeyFromString(shortcut)
 	
 	def setKeyFromString(self, s):
+		s = s.replace('<control>', 'Ctrl+').replace('<alt>', 'Alt+').replace('<shift>', 'Shift+')
 		self.keyChar = s.split('+')[-1]
 		modifiers = s.split('+')[0:-1]
 		for item in modifiers:
@@ -235,15 +163,10 @@ class KeyBinding(object):
 			raise ValueError('key should be upper case')
 			
 	def getKeyString(self):
-		s = ''
-		if self.control:
-			s += 'Ctrl+'
-		if self.alt:
-			s += 'Alt+'
-		if self.shift:
-			s += 'Shift+'
-		s += self.keyChar
-		return s
+		s = 'Ctrl+' if self.control else ''
+		s += 'Alt+' if self.alt else ''
+		s += 'Shift+' if self.shift else ''
+		return s + self.keyChar
 		
 	def getSortKey(self):
 		return (self.keyChar, self.control, self.alt, self.shift, self.priority, self.command)
@@ -255,14 +178,8 @@ def addBindingsManual(bindings, s):
 	lines = s.replace('\r\n', '\n').split('\n')
 	for line in lines:
 		if line.strip():
-			try:
-				modifiersAndKey, command, priority, platform, setName = line.split('|')
-				binding = KeyBinding(setName, command, priority=int(priority), platform=platform)
-				binding.setKeyFromString(modifiersAndKey)
-				bindings.append(binding)
-			except ValueError:
-				print(line)
-				raise
+			keys, command, priority, platform, setName = line.split('|')
+			bindings.append(KeyBinding(setName, command, keys, priority=int(priority), platform=platform))
 
 def retrieveCodeLines(filename, startingLine, endingLine, mustInclude=None):
 	allLines = readall(filename, 'rb').replace('\r\n', '\n').split('\n')
@@ -289,7 +206,31 @@ def retrieveCodeLines(filename, startingLine, endingLine, mustInclude=None):
 
 	return allLines[linesThatMatchStart[0]:endLine + 1]
 
-def readall(filename, mode='r'):
+def takePairs(iterable):
+	import itertools
+	it = iter(iterable)
+	item = list(itertools.islice(it, 2))
+	while item:
+		yield item
+		item = list(itertools.islice(it, 2))
+
+def warn(prompt):
+	print(prompt)
+	while True:
+		s = getInput('Continue? y/n')
+		if s == 'y':
+			break
+		elif s == 'n':
+			raise RuntimeError('chose not to continue')
+
+def getInput(prompt):
+	import sys
+	if sys.version_info[0] <= 2:
+		return raw_input(prompt)
+	else:
+		return input(prompt)
+
+def readall(filename, mode='rb'):
 	with open(filename, mode) as f:
 		return f.read()
 
@@ -308,59 +249,31 @@ def assertEqArray(expected, received):
 		assertEq(repr(expected[i]), repr(received[i]))
 
 def tests():
-	mockProperties = '''
-customcommand.test.name=Test Custom
-customcommand.test.shortcut=Shift+F12
-
-#customcommand.commented.name=Test cmtd
-#customcommand.commented.shortcut=F1
-
-user.shortcuts=\
-Ctrl+Shift+V|IDM_PASTEANDDOWN|\
-Ctrl+PageDown|IDM_NEXTFILE|
-
+	propstring = r'''
+a=b=c
+test.expand=$(span)
 if PLAT_GTK
-	*language.sql=S&QL|sql||
-	*language.html=H&TML|html|Control+Alt+Shift+F11|
-	*language.php=P&HP|php|$(keyMissing)|
-	*language.xml=&XML|xml|$(keyXML)|
-	keyXML=Alt+Shift+/
+	plat=gtk
 if PLAT_WIN
-	user.shortcuts=
-	*language.xml=&XML|xml|Ctrl+O|
-
-fff=*
-command.name.11.$(fff)=Test Aaa
-command.11.$(fff)=dostuff(x,y,z)
-command.shortcut.11.$(fff)=Ctrl+Aaa
-
-command.name.12.*=Test Bbb
-command.12.*=dostuff(x,y,z)
-command.shortcut.12.*=Ctrl+Bbb
-
-command.name.4.$(fff)=Test Ddd
-
-command.name.5.*.y=Test Ccc
-command.shortcut.5.*.y=Shift+Tab'''
-
+	plat=win
+span=a\
+b\
+c
+testfileendswithslash=test''' + '\\'
 	props = PropSetFile('gtk')
-	props.ReadString(mockProperties)
-	results = readFromProperties(props)
-	results.sort(key=lambda obj: obj.getSortKey())
+	props.ReadString(propstring)
+	assertEq('b=c', props.GetString('a'))
+	assertEq('abc', props.GetString('span'))
+	assertEq('abc', props.Expanded(props.GetString('test.expand')))
+	assertEq('gtk', props.GetString('plat'))
+	assertEq('test', props.GetString('testfileendswithslash'))
 
-	expected = '''Alt+Shift+/|set language XML|40|any|properties *language
-Ctrl+4|Test Ddd|50|any|properties command (implicit)
-Ctrl+Aaa|Test Aaa|50|any|properties command
-Ctrl+Bbb|Test Bbb|50|any|properties command
-Ctrl+Alt+Shift+F11|set language HTML|40|any|properties *language
-Shift+F12|Test Custom|50|any|properties command
-Ctrl+PageDown|IDM_NEXTFILE|60|any|properties user.shortcuts
-Shift+Tab|Test Ccc|50|*.y|properties command
-Ctrl+Shift+V|IDM_PASTEANDDOWN|60|any|properties user.shortcuts'''
-	expectedArr = []
-	addBindingsManual(expectedArr, expected)
-	assertEqArray(expectedArr, results)
-	
+	dir = '../scite_main_props'
+	props = getAllProperties(dir, 'gtk')
+	bindings = readFromProperties(props)
+	bindings.sort(key=lambda item:item.getSortKey())
+	for b in bindings:
+		print repr(b)
 	
 if __name__ == '__main__':
 	tests()
