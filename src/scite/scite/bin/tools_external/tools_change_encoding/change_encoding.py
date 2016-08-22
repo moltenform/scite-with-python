52b96683a6d61900d44e5881ed7331f86a074d5c
Ben Fisher <scitewiki@gmail.com>
2016-08-22 09:52:58 -0700

    New feature: plugin to change character encoding.

---
+++ src/scite/scite/bin/tools_external/tools_change_encoding/change_encoding.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import codecs
+
+def getRawInput(prompt):
+    print(prompt)
+    sys.stdout.flush()
+    if sys.version_info[0] <= 2:
+        return raw_input('')
+    else:
+        return input('')
+
+def askForCodec(prompt, defaultCodec):
+    while True:
+        scodec = getRawInput('\n' + prompt).strip()
+        if scodec == 'q':
+            return None
+
+        try:
+            scodec = scodec or defaultCodec
+            codecs.lookup(scodec)
+            return scodec
+        except LookupError:
+            print('Could not find this codec (' + scodec + ')')
+    
+def go(path):
+    print('Change encoding.\nChanges the current file,\nit might make sense to keep a backup copy.')
+    source = askForCodec('Please type the current encoding and press Enter \n(default=cp1252, q to exit):', 'cp1252')
+    if source is None:
+        return
+        
+    dest = askForCodec('Please type the destination encoding and press Enter \n(default=utf-8, q to exit):', 'utf-8')
+    if dest is None:
+        return
+        
+    with codecs.open(path, 'rb', source) as fin:
+        alltxt = fin.read()
+    
+    with codecs.open(path, 'wb', dest) as fout:
+        fout.write(alltxt)
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
+        go(sys.argv[1])
