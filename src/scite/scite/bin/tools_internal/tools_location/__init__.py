6fb2d58f444c31b4afb07c84b50e1838ada383a4
moltenform.com (Ben Fisher) <amoltenform@gmail.com>
2020-07-08 10:06:49 -0700

    new feature: reopen closed tab Ctrl-Shift-T

--- src/scite/scite/bin/tools_internal/tools_location/__init__.py
+++ src/scite/scite/bin/tools_internal/tools_location/__init__.py
@@ -3,92 +3,54 @@
 # Released under the GNU General Public License version 3
 
 from scite_extend_ui import ScApp, ScEditor, ScConst
+import os
 
-# if this is turned on, you'll be sent to the end or start of the file, which was less convenient to use.
-okToBeTakenToEndOfFile = False
+# ctrl-shift-j to move to start/end of block
+from goblocklocation import *
 
+# ctrl-shift-o to open selected text
+from openselectedtext import *
+
+# location tracking is a custom feature for scite-with-python, but it's implemented in c++
 def LocationNext():
     ScApp.LocationNext()
 
 def LocationPrev():
     ScApp.LocationPrev()
 
-def GetPositionOfStartOfBlock(posToWorkWith):
-    bitmask = ScConst.SC_FOLDLEVELNUMBERMASK
-    previousLine = ScEditor.LineFromPosition(posToWorkWith)
-    previousLevel = ScEditor.GetFoldLevel(previousLine) & bitmask
-    line = previousLine
-    
-    # search for a line where the level is less than the current level.
-    found = False
-    while line > 0:
-        if (ScEditor.GetFoldLevel(line) & bitmask) < previousLevel:
-            found = True
-            break
-        line -= 1
-    
-    if not found and not okToBeTakenToEndOfFile:
-        return None, None
-    else:
-        return line, ScEditor.PositionFromLine(line)
+# ctrl-shift-t to re-open a closed tab
+# useful because it's not limited to 20 entries
+gClosedTabs = []
+gSavedSep = ord(' ')
+gSavedMaxHeight = 1
+def OnClose(file):
+    if file not in gClosedTabs and os.path.isfile(file):
+        gClosedTabs.insert(0, file)
 
-def GoUp(addToSelection):
-    previousSelStart = ScEditor.GetSelectionStart()
-    previousSelEnd = ScEditor.GetSelectionEnd()
-    line, newpos = GetPositionOfStartOfBlock(previousSelStart)
-    if line is None:
-        return
-    
-    ScEditor.GotoPos(newpos)
-    if addToSelection:
-        ScEditor.SetSelectionStart(ScEditor.PositionFromLine(line))
-        ScEditor.SetSelectionEnd(previousSelEnd)
-    else:
-        ScEditor.LineEnd()
+def OnOpen(file):
+    # remove it from the list if it's opened in any form
+    global gClosedTabs
+    gClosedTabs = [item for item in gClosedTabs if item != file]
 
-def GoDown(addToSelection):
-    bitmask = ScConst.SC_FOLDLEVELNUMBERMASK
-    lastLineInDocument = ScEditor.LineFromPosition(ScEditor.GetTextLength())
-    previousSelStart = ScEditor.GetSelectionStart()
-    previousSelEnd = ScEditor.GetSelectionEnd()
-    
-    # why move the caret down a line? for the case where the current line is "while(condition) {"
-    # it's convenient to act as if the caret were inside the body of this block, even though
-    # technically it is outside.
-    # this doesn't work as well for blocks that are only two lines long, preventing
-    # Ctrl+Shift+J, Ctrl+Shift+K from selecting the entire block as expected, but the tool works well enough for now.
-    ScEditor.LineDown()
-    
-    startScopeLine, startScopePos = GetPositionOfStartOfBlock(ScEditor.GetSelectionStart())
-    if startScopeLine is None:
-        ScEditor.SetSelectionStart(previousSelStart)
-        ScEditor.SetSelectionEnd(previousSelEnd)
-        return
-    
-    # fold levels are apparently not determined until user views that text.
-    # call EnsureChildrenVisible, in order to both compute fold levels for the rest of the scope and expand everything.
-    ScEditor.SetCurrentPos(startScopePos)
-    ScApp.CmdExpandEnsureChildrenVisible()
-    
-    startScopeLevel = ScEditor.GetFoldLevel(startScopeLine) & bitmask
-    line = startScopeLine + 1
-    found = False
-    while line < lastLineInDocument:
-        if (ScEditor.GetFoldLevel(line + 1) & bitmask) <= startScopeLevel:
-            found = True
-            break
-        line += 1
-    
-    if not found and not okToBeTakenToEndOfFile:
-        ScEditor.SetSelectionStart(previousSelStart)
-        ScEditor.SetSelectionEnd(previousSelEnd)
-        return
-    
-    ScEditor.GotoPos(ScEditor.PositionFromLine(line))
-    ScEditor.LineEnd()
-    if addToSelection:
-        endPos = ScEditor.GetSelectionEnd()
-        ScEditor.SetSelectionStart(previousSelStart)
-        ScEditor.SetSelectionEnd(endPos)
-
+def reopenClosedTab():
+    global gSavedSep, gSavedMaxHeight
+    if len(gClosedTabs) > 0:
+        gSavedSep = ScEditor.GetAutoCSeparator()
+        gSavedMaxHeight = ScEditor.GetAutoCMaxHeight()
+        itemList = '\n'.join(gClosedTabs)
+        ScEditor.SetAutoCSeparator(ord('\n'))
+        ScEditor.SetAutoCMaxHeight(20)
+        ScEditor.UserListShow(ScEditor.UserListIDs.reopenClosedTabs, itemList)
+            
+def OnUserListSelection(chosen, id):
+    global gClosedTabs
+    if id == ScEditor.UserListIDs.reopenClosedTabs:
+        ScEditor.SetAutoCSeparator(gSavedSep)
+        ScEditor.SetAutoCMaxHeight(gSavedMaxHeight)
+        if os.path.isfile(chosen):
+            # remove it from the list -- for example if filename is invalid, ensures it is no longer in the list
+            gClosedTabs = [item for item in gClosedTabs if item != chosen]
+            ScApp.OpenFile(chosen)
+        else:
+            print("No longer exists: " + chosen)
 
